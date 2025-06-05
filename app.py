6842218e161df020119caf41317987da5b8fca25
redspacecat <162653322+redspacecat@users.noreply.github.com>
2025-06-04 20:58:11 -0700

    add pfp loader

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ from keep_alive import keep_alive
 
 session_id = os.environ.get("SESSION_ID")
 session = sa.login_by_id(session_id, username=os.environ.get("USERNAME")) #replace with your session_id and username
-cloud = session.connect_cloud(992640266) #replace with your project id
-client = cloud.requests()
+cloud1 = session.connect_cloud(992640266) #replace with your project id
+client1 = cloud1.requests()
+cloud2 = session.connect_cloud(996217996)
+client2 = cloud2.requests()
 
-@client.request
+@client1.request
 def ping(): #called when client receives request
     print("Ping request received")
     return "pong"
 
-@client.request
+@client1.request
 def message_count(username): #called when client receives request
     print("Getting message count for", username)
     try:
@@ -23,9 +25,45 @@ def message_count(username): #called when client receives request
         return "Error"
     return count
 
-@client.event
+@client1.event
 def on_ready():
-    print("Request handler is running")
+    print("Request handler for message count is running")
+
+
+@client2.request
+def ping(): #called when client receives request
+    print("Ping request received")
+    return "pong" #sends back 'pong' to the Scratch project
+
+@client2.request
+def pfp(username, resolution):
+    try:
+        print(f"Profile picture requested for {username}")
+
+        user = sa.get_user(username).id
+        url = "https://uploads.scratch.mit.edu/get_image/user/" + str(user) + "_100x100.png"
+        urllib.request.urlretrieve(url, "avatar.png")
+
+        img = Image.open("avatar.png").convert("RGBA")
+        img = img.resize((int(resolution), int(resolution)))
+        width, height = img.size
+        pixels = img.load()
+
+        colors = []
+        for y in range(height):
+            for x in range(width):
+                r, g, b, a = pixels[x, y]
+                color = a * 16777216 + r * 65536 + g * 256 + b
+                colors.append(color)
+        return colors
+    except sa.exceptions.UserNotFound:
+        return "User Not Found"
+
+@client2.event
+def on_ready():
+    print("Request handler for pfp loader is running")
 
 keep_alive()
-client.start(thread=True) # thread=True is an optional argument. It makes the cloud requests handler run in a thread
+client1.start()
+client2.start()
+print("Started stuff")
