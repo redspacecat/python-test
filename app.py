83695c692ebdc7bf598a312ee5c59201f15df056
redspacecat <162653322+redspacecat@users.noreply.github.com>
2025-06-04 19:36:36 -0700

    try with more stuff

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+import scratchattach as sa
+import requests
+import json, os
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+session_id = os.environ.get("SESSION_ID")
+session = sa.login_by_id(session_id, username=os.environ.get("USERNAME")) #replace with your session_id and username
+cloud = session.connect_cloud(1184958260) #replace with your project id
+client = cloud.requests()
+
+@client.request
+def ping(): #called when client receives request
+    print("Ping request received")
+    return "pong"
+
+@client.request
+def message_count(username): #called when client receives request
+    print("Getting message count for", username)
+    # try:
+    r = requests.get(f"https://api.scratch.mit.edu/users/{username}/messages/count")
+    count = json.loads(r.text)["count"]
+    # except:
+    # return "There was an error while fetching message count"
+    return count
+
+@client.event
+def on_ready():
+    print("Request handler is running")
+
+client.start(thread=True) # thread=True is an optional argument. It makes the cloud requests handler run in a thread
